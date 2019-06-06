404d381e60788289c6fc3c66a94b44869ed81586
Valerii <www.l1nk32@gmail.com>
2019-06-06 17:17:48 +0300

    Initial Commit

---
+++ carTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  carTask
+//
+//  Created by Valerii on 06.06.2019.
+//  Copyright © 2019 Valerii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
